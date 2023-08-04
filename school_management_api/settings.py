1c5500cc35d63d4c3751b3b15f65a169601a6d70
Edy <73756874+edypnog@users.noreply.github.com>
2023-08-04 18:02:02 -0300

    Update settings.py

--- school_management_api/settings.py
+++ school_management_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s0830dq=j531@b+&ri7t^x9@ejez%&b24ldui5uatahagm*7(@"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
